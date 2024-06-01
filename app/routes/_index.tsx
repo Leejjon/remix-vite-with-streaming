52b80346196bb061391374bdd2f38cb98525dc80
Leejjon <leejjon@gmail.com>
2024-06-01 21:22:42 +0200

    Pokemon streaming.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,88 +1,50 @@
-import { defer, type MetaFunction } from "@remix-run/node";
+import { defer } from "@remix-run/node";
 import { Await, useLoaderData } from "@remix-run/react";
+import pokemonJsonAsAny from "../pokemon.json";
 import { Suspense } from "react";
-import { MatchesResponse} from "~/model/Match";
 
-const {REACT_APP_FOOTBALL_API_KEY} = process.env;
+type PokemonResponse = {id: number, name: string};
+type PokemonNameAndUrl = {name: string, url: string};
+type PokemonJsonType = {results: Array<PokemonNameAndUrl>};
 
-export type Feriado = {
-  name: string;
-  date: string;
-};
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
 
-export type Endereco = {
-  cep: string;
-  state: string;
-  city: string;
-  neighborhood: string;
-  street: string;
-};
+async function loadPokemon(pokemonId: number, pokemon: PokemonNameAndUrl): Promise<PokemonResponse> {
 
-// function delay(ms: number) {
-//   return new Promise((resolve) => setTimeout(resolve, ms));
-// }
+    await delay(pokemonId * 100)
+    return {id: pokemonId, name: pokemon.name} as PokemonResponse;
+}
 
 export async function loader() {
-  if (!REACT_APP_FOOTBALL_API_KEY) {
-    // You can just register on api.football-data.org to generate your own free key.
-    throw new Error("Please put your own key in the .env.local file as REACT_APP_FOOTBALL_API_KEY value");
-  }
+    const pokemonJson = pokemonJsonAsAny as PokemonJsonType;
+    const pokemonList = pokemonJson.results.map((value, index) => {
+        return loadPokemon(++index, value);
+    });
 
-  const fetchOptions: RequestInit = {
-    method: 'GET',
-    headers: {
-      'X-Auth-Token': REACT_APP_FOOTBALL_API_KEY
-    }
-  };
-
-  const response: Promise<MatchesResponse> = fetch("https://api.football-data.org/v4/competitions/2003/matches", fetchOptions).then((res) => res.json());
-
-  const feriados: Promise<Feriado[]> = fetch(
-    "https://brasilapi.com.br/api/feriados/v1/2023"
-  ).then((res) => res.json());
-
-  return defer({
-    feriados,
-    response
-  });
+    return defer({...pokemonList});
 }
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+export default function Pokemon() {
+    const data = useLoaderData<typeof loader>();
 
-export default function Index() {
-  const {feriados, response} =
-    useLoaderData<typeof loader>();
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <Suspense fallback={<p>loading</p>}>
-        <Await resolve={feriados}>
-          {(feriados) => (
-            <ul>
-              {feriados.map((feriado) => (
-                <li key={feriado.date}>{feriado.name}</li>
-              ))}
-            </ul>
-          )}
-        </Await>
-      </Suspense>
-      <Suspense fallback={<p>Eredivisie</p>}>
-        <Await resolve={response} errorElement={<p>Error</p>}>
-          {(response) => (
-            <ul>
-              {response.matches.map((match) => (
-                <li key={match.id}>{match.homeTeam.name} - {match.awayTeam.name}</li>
-              ))}
-            </ul>
-          )}
-        </Await>
-      </Suspense>
-    </div>
-  );
+    const suspenseList = Object.values(data).map((value, index) => {
+        return (
+            <Suspense key={index} fallback={<p>loading</p>}>
+                <Await resolve={value} errorElement={<p>Error</p>}>
+                    {(response) => (
+                        <p>{response.id} - {response.name}</p>
+                    )}
+                </Await>
+            </Suspense>
+        );
+    });
+    
+    return (
+        <>
+            <div>All first gen pokemon:</div>
+            {suspenseList}
+        </>
+    );
 }
-
